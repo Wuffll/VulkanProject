ddd348bbf1cda524777c44e63a584af8a6a9be37
Adam E. <adam.ergotic@outlook.com>
2026-07-19 19:15:00 +0200

    Confirm tested lib includes

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -1,9 +1,25 @@
 
+#include <format>
+#include <iostream>
+
 #include "DummyLibrary/DummyLibrary.h"
+#include "Logger/Logger.h"
+#include "assimp/vector3.h"
+
+std::string Vec3ToString(const aiVector3f& InVector)
+{
+    return std::format("({0}, {1}, {2})", InVector.x, InVector.y, InVector.z);
+}
 
 int main()
 {
     VulkanApp::DummyLibrary::DummyFunction();
+
+
+    const aiVector3f vector{0.0f};
+
+    std::cout << Vec3ToString(vector) << std::endl;
+    Logger::Log(LogLevel::Info, "Testing");
 
     return 0;
 }
